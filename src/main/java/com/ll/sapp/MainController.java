3827d6e2e123002f417a8ab2c50fbff1699fafeb
starringg <banabana0504@gmail.com>
2024-07-30 17:32:36 +0900

    work

--- src/main/java/com/ll/sapp/MainController.java
+++ src/main/java/com/ll/sapp/MainController.java
@@ -11,4 +11,9 @@ public class MainController {
     public String index() {
         return "안녕";
     }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
 }
