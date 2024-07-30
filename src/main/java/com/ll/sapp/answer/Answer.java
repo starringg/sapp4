889423df291bc0625c976934d2b486a205091428
starringg <banabana0504@gmail.com>
2024-07-30 13:36:32 +0900

    work

---
+++ src/main/java/com/ll/sapp/answer/Answer.java
@@ -0,0 +1,25 @@
+package com.ll.sapp.answer;
+
+import com.ll.sapp.question.Question;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
---
+++ src/main/java/com/ll/sapp/answer/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.ll.sapp.answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
